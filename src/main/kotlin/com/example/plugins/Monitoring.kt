725d5cbc8faceb641e4709ed5dff4238c1360ba8
Pascal Nitsche <saikootau@gmail.com>
2021-07-01 01:53:44 +0200

    Add call id support

--- src/main/kotlin/com/example/plugins/Monitoring.kt
+++ src/main/kotlin/com/example/plugins/Monitoring.kt
@@ -2,12 +2,15 @@ package com.example.plugins
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.metrics.micrometer.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.micrometer.prometheus.*
 import org.slf4j.event.*
+import java.util.*
+import java.util.regex.Pattern
 
 val Application.micrometerRegistry: PrometheusMeterRegistry by lazy {
     PrometheusMeterRegistry(PrometheusConfig.DEFAULT).also { registry ->
@@ -22,9 +25,19 @@ fun Application.configureMonitoring() {
     install(MicrometerMetrics) {
         registry = micrometerRegistry
     }
+    install(CallId) {
+        retrieveFromHeader(HttpHeaders.XRequestId)
+        generate { UUID.randomUUID().toString() }
+        verify {
+            Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\$")
+                .matcher(it)
+                .matches()
+        }
+    }
     install(CallLogging) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
+        callIdMdc("callId")
     }
 
     routing {
